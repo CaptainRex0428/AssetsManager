1fb56470b445cdf6af63fd573763b4df0e058a8d
luogongkai <luogongkai@digisky.com>
2024-08-13 17:28:24 +0800

    #Update Module Unload

--- Source/AssetsManager/Private/AssetsManager.cpp
+++ Source/AssetsManager/Private/AssetsManager.cpp
@@ -25,6 +25,7 @@ void FAssetsManagerModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+	FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(FName(CONTENTFOLDERMANAGERTABNAME));
 }
 
 #pragma region ContentBrowserMenuExtend
