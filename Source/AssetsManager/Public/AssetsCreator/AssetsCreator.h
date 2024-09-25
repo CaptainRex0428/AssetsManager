d3440eb5e27c20375062780f2720b67d245990a3
luogongkai <luogongkai@digisky.com>
2024-09-25 10:09:52 +0800

    #Update Assets Handler

--- Source/AssetsManager/Public/AssetsCreator/AssetsCreator.h
+++ Source/AssetsManager/Public/AssetsCreator/AssetsCreator.h
@@ -7,40 +7,24 @@
 
 #include "ConfigManager.h"
 
-#include "AssetsCreator.generated.h"
-
-UENUM(BlueprintType)
-enum class TextureChannel : uint8
-{
-	Red=0 UMETA(DisplayName="Red"),
-	Green=1 UMETA(DisplayName = "Green"),
-	Blue=2 UMETA(DisplayName = "Blue"),
-	Alpha=3 UMETA(DisplayName = "Alpha")
-};
-
 /*
  * 
  */
-UCLASS()
 class ASSETSMANAGER_API UAssetsCreator : public UAssetActionUtility
 {
-	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable,Category = "AssetsCreator|Material")
 	static UMaterialInstanceConstant* CreateMaterialInstanceConstant(
 		FString PackageFolder,
 		FString AssetName,
 		UMaterialInterface* MaterialParent);
 
-	UFUNCTION(BlueprintCallable, Category = "AssetsCreator|Material")
 	static bool ToggleMaterialInstanceConstantStaticSwitch(
 		UMaterialInstanceConstant* MaterialInstance,
 		FString ParameterName,
 		EMaterialParameterAssociation Association = EMaterialParameterAssociation::GlobalParameter
 	);
 
-	UFUNCTION(BlueprintCallable, Category = "AssetsCreator|Material")
 	static bool GetMaterialInstanceConstantStaticSwitch(
 		UMaterialInstanceConstant* MaterialInstance,
 		FString ParameterName,
---
+++ Source/AssetsManager/Public/AssetsHandler.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetActionUtility.h"
+#include "ConfigManager.h"
+#include "AssetsChecker/AssetsChecker.h"
+#include "AssetsCreator/AssetsCreator.h"
+
+#include "AssetsHandler.generated.h"
+
+UENUM(BlueprintType)
+enum class TextureChannel : uint8
+{
+	Red = 0 UMETA(DisplayName = "Red"),
+	Green = 1 UMETA(DisplayName = "Green"),
+	Blue = 2 UMETA(DisplayName = "Blue"),
+	Alpha = 3 UMETA(DisplayName = "Alpha")
+};
+
+/**
+ * 
+ */
+UCLASS()
+class ASSETSMANAGER_API UAssetsHandler : public UAssetActionUtility
+{
+	GENERATED_BODY()
+	
+#pragma region CallInEditor
+
+	UFUNCTION(CallInEditor, Category = "AssetsHandler")
+	void CreateIDCheckerMaterial(
+		TextureChannel Channel,
+		int32 UVs);
+
+	UFUNCTION(CallInEditor, Category = "AssetsHandler")
+	void DuplicateAssets(
+		int NumOfDupicates,
+		bool forced = true);
+
+	UFUNCTION(CallInEditor, Category = "AssetsHandler")
+	void RemoveUnusedAssets();
+
+	UFUNCTION(CallInEditor, Category = "AssetsHandler")
+	void ReplaceName(
+		const FString& OriginStr,
+		const FString& ReplaceStr);
+
+#pragma endregion
+
+#pragma region CallInEditor
+
+#pragma endregion
+
+};
