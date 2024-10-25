61708908578a6b64ddae1f23f35490163bbd2ab8
luogongkai <luogongkai@digisky.com>
2024-10-25 16:29:10 +0800

    #Update Sync Proj

---
+++ Source/AssetsManager/Private/DSAssetsHandler.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DSAssetsHandler.h"
+
+#include "ManagerLogger.h"
+#include "StandardAsset/FCustomStandardAssetData.h"
+#include "StandardAsset/FCustomStandardTexture2DData.h"
+
+#include "EditorUtilityLibrary.h"
+#include "EditorAssetLibrary.h"
+
+#include "FileHelpers.h"
+
+#include "ObjectTools.h"
+#include "AssetToolsModule.h"
+#include "AssetRegistry/AssetRegistryModule.h"
---
+++ Source/AssetsManager/Public/DSAssetsHandler.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetActionUtility.h"
+#include "ConfigManager.h"
+#include "AssetsHandler.h"
+
+#include "DSAssetsHandler.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSETSMANAGER_API UDSAssetsHandler : public UAssetsHandler
+{
+	GENERATED_BODY()
+	
+};
